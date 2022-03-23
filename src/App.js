fa7242508d85fe79254a43168c4df6c6887f8a2d
Linear-DX <mariolinear97@gmail.com>
2022-03-23 15:42:58 +0700

    adding class playlist in app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data/single-sample.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
+        <div class="playlist">
+          <img src={data.album.images[1].url} />
+          <h4>{data.album.name}</h4>
+          <p>{data.album.artists[0].name}</p>
+          <button type="submit">Select</button>
+        </div>
       </header>
     </div>
   );
